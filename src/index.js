f8f0d307de9adde755156aad1d87894ccdfa3550
cse18107 <cse18107@cemk.ac.in>
2024-07-13 12:00:28 +0530

    added conenct db call

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 const fastify = require('fastify')({ logger: true }); // calling the fastify constructor
 const app = require('./app');
+const connectToDB = require('./config/dbConfig');
 const serverConfig = require('./config/serverConfig')
 
 fastify.register(app);
 
-fastify.listen({ port: serverConfig.PORT }, (err) => {
+fastify.listen({ port: serverConfig.PORT }, async (err) => {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
   }
+  await connectToDB();
   console.log(`Server up at port ${serverConfig.PORT}`);
 });
 

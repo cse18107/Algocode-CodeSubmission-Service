67700b5a7ca1a69adf5b9eff4411e965338565ec
cse18107 <cse18107@cemk.ac.in>
2024-07-14 16:02:47 +0530

    added axios instance for communicating other service

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -6,5 +6,6 @@ module.exports = {
   REDIS_PORT: process.env.REDIS_PORT || "6379",
   REDIS_HOST: process.env.REDIS_HOST || "127.0.0.1",
   ATLAS_DB_URL: process.env.ATLAS_DB_URL,
-  NODE_ENV: process.env.NODE_ENV
+  NODE_ENV: process.env.NODE_ENV,
+  PROBLEM_ADMIN_SERVICE_URL: process.env.PROBLEM_ADMIN_SERVICE_URL,
 };
--- src/services/submissionService.js
+++ src/services/submissionService.js
@@ -1,3 +1,4 @@
+const { fetchProblemDetails } = require('../apis/problemAdminApi');
 const SubmissionProducer = require('../producers/submissionQueueProducer');
 
 class SubmissionService {
@@ -11,14 +12,38 @@ class SubmissionService {
     }
 
     async addSubmission(submissionPayload) {
+        // Hit the problem admin service and fetch the problem details
+        const problemId = submissionPayload.problemId;
+
+        const problemAdminApiResponse = await fetchProblemDetails(problemId);
+
+        if(!problemAdminApiResponse) {
+            throw { message: "Not able to create submission" };
+        }
+
+        const languageCodeStub = problemAdminApiResponse.data.codeStubs.find(codeStub => codeStub.language.toLowerCase() === submissionPayload.language.toLowerCase());
+        
+        // console.log(languageCodeStub)
+
+        submissionPayload.code = languageCodeStub.startSnippet + "\n\n" + submissionPayload + "\n\n" + languageCodeStub.endSnippet
+
+        
+        // we are going to create the entry in db
         const submission =
          await this.submissionRepository.createSubmission(submissionPayload);
         if(!submission){
             // TODO: add error handling here
             throw { message: "Not able to create submission"}
         }
-        console.log(submission);
-        const response = await SubmissionProducer(submission);
+        // console.log(submission);
+        const response = await SubmissionProducer({
+          [submission._id]: {
+            code: submission.code,
+            language: submission.language,
+            inputCase: problemAdminApiResponse.data.testCases[0].input,
+            outputCase: problemAdminApiResponse.data.testCases[0].output,
+          },
+        });
         return {queueResponse: response, submission};
     }
 }
